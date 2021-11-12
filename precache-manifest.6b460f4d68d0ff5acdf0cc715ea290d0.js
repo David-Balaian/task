86bfae729993918c227165765ce4af37f206a80e
David-Balaian <david@navasolutions.com>
2021-11-12 05:15:02 +0400

    Updates

---
+++ precache-manifest.6b460f4d68d0ff5acdf0cc715ea290d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4377aa30f681811f88b0130e29f7957b",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "6fd0e0f437583ec490de",
+    "url": "/task/static/css/main.bea85a39.chunk.css"
+  },
+  {
+    "revision": "ab3577593f1eb4ab99c1",
+    "url": "/task/static/js/2.81f66c85.chunk.js"
+  },
+  {
+    "revision": "b6cf2454b4e98d6764c81cf44844e4d3",
+    "url": "/task/static/js/2.81f66c85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd0e0f437583ec490de",
+    "url": "/task/static/js/main.9e25b220.chunk.js"
+  },
+  {
+    "revision": "e5284e5de2396dc1be1f",
+    "url": "/task/static/js/runtime-main.cf74d6d5.js"
+  }
+]);
